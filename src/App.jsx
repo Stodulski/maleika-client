a04cf0d1c55bb6de3eb2e026fac83be092cf6215
Stodulski <maxisto043@gmail.com>
2024-07-31 19:02:24 -0300

    Agregado de vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,11 @@ export const App = () => {
                     {isAuthenticated && (
                         <>
                             <Route exact path="/panel" element={<Panel />} />
-                            
+                            <Route
+                                exact
+                                path="/login"
+                                element={<Navigate to={"/panel"} />}
+                            />
                         </>
                     )}
                     <Route exact path="/login" element={<Login />} />
